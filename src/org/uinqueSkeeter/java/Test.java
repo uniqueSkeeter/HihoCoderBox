f1bc021adaeae9b1d9b710feb3a5402252100541
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-08 21:10:44 +0800

    init

---
+++ src/org/uinqueSkeeter/java/Test.java
@@ -0,0 +1,10 @@
+package org.uinqueSkeeter.java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world");
+	}
+
+}
